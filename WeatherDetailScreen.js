de1e067ee9fbd058a099024ad448cebe8a08ab08
solidw <solidw@naver.com>
2019-06-12 00:08:32 +0900

    fix : Modify react.stylesheets

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -23,7 +23,8 @@ export default class WeatherDetailScreen extends React.Component {
     const city = navigation.getParam('city', null);
     // const city = 'Daejeon';
 
-    fetch(`http://10.0.2.2:8080/byCityName/${city}`)
+    fetch(`http://10.0.2.2:8080/byCityName/${city}`) // localhost in AVD
+    // fetch(`http://wantae.cf/byCityName/Daejeon`) // localhost in AVD
     .then(response => response.json())
     .then(info => {
       this.setState({
@@ -36,100 +37,99 @@ export default class WeatherDetailScreen extends React.Component {
   render() {
     if (this.state.isLoading) {
       return (
-
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>데이터를 불러오는 중입니다.</Text>
+        <Text style={styles.content}>데이터를 불러오는 중입니다.</Text>
         </View>
       )
     }
     let celsius = this.state.main.temp - 273.15;
     let humidity = this.state.main.humidity;
-    let w = this.state.weather[0].icon + "";
-    let src;
+    var w = this.state.weather[0].icon + "";
+    src = './assets/' + w + "png";
     if(w =='01d' || w== '01n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
-        <Image style={styles.imageSetting} source={require('./assets/01d.png')} />
-        <Text style={styles.fontsSetting}>태양아 저 나그네의 옷을 벗겨라</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
+        <Image style={{width:'40%', height:'40%', resizeMode:'contain'}} source={require('./assets/02d.png')} />
+        <Text style={styles.comment}>태양아 저 나그네의 옷을 벗겨라</Text>
         </View>
       );
     }else if(w =='02d' || w== '02n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/02d.png')} />
-        <Text style={styles.fontsSetting}>비가 내릴까 말까...</Text>
+        <Text style={styles.comment}>비가 내릴까 말까...</Text>
         </View>
       );
     }else if(w=='03d' || w== '03n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/03d.png')} />
-        <Text style={styles.fontsSetting}>우산 챙기면 비 안옵니다,</Text>
+        <Text style={styles.comment}>우산 챙기면 비 안옵니다,</Text>
         </View>
       );
     }else if(w=='04d' || w=='04n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/04d.png')} />
-        <Text style={styles.fontsSetting}>무서운 천둥번개가 칠지도 몰라요.</Text>
+        <Text style={styles.comment}>무서운 천둥번개가 칠지도 몰라요.</Text>
         </View>
       );
     }else if(w=='09d' || w== '09n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/09d.png')} />
-        <Text style={styles.fontsSetting}>미세먼지를 쓸어갈 달콤한 빗물이네요</Text>
+        <Text style={styles.comment}>미세먼지를 쓸어갈 달콤한 빗물이네요</Text>
         </View>
       );
     }
     else if(w=='10d' || w== '10n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/10d.png')} />
-        <Text style={styles.fontsSetting}>여우비</Text>
+        <Text style={styles.comment}>여우비</Text>
         </View>
       );
     }
     else if(w=='11d' || w== '11n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/11d.png')} />
-        <Text style={styles.fontsSetting}>토르가 옵니다</Text>
+        <Text style={styles.comment}>토르가 옵니다</Text>
         </View>
       );
     }
     else if(w=='13d' || w== '13n'){
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/13d.png')} />
-        <Text style={styles.fontsSetting}>나랑 눈사람 만들래?</Text>
+        <Text style={styles.comment}>나랑 눈사람 만들래?</Text>
         </View>
       );
     }
     else {
       return (
         <View style={styles.container}>
-        <Text style={styles.fontsSetting}>온도: {celsius.toFixed(1)}</Text>
-        <Text style={styles.fontsSetting}>습도: {humidity+"%"}</Text>
-        <Text style={styles.fontsSetting}>가끔은 모르고 나가도 좋은 날이 있어요</Text>
-        <Text style={styles.fontsSetting}>아마 쨍쨍할거에요</Text>
+        <Text style={styles.content}>온도: {celsius.toFixed(1)}</Text>
+        <Text style={styles.content}>습도: {humidity+"%"}</Text>
         <Image style={styles.imageSetting} source={require('./assets/01d.png') } />
+        <Text style={styles.comment}>가끔은 모르고 나가도 좋은 날이 있어요</Text>
+        <Text style={styles.comment}>아마 쨍쨍할거에요</Text>
         </View>
       );
     }
@@ -143,13 +143,18 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
   },
-  fontsSetting: {
+  content: {
+    justifyContent:'center',
+    alignItems:'center',
     fontSize:35,
     textAlign:'center',
-    borderWidth:1,
+  },
+  comment: {
+    fontSize:20,
   },
   imageSetting: {
-    width:200,
-    height:150,
+    // width:200,
+    // height:150,
+    borderWidth:1,
   },
 });
